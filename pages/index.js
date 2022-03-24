b10f9745c1f7e6367478f2f947950d402623f353
Mauro Sosa <mauro4sosa@gmail.com>
2022-03-24 17:38:24 -0300

    index.js modified

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,6 @@ export default function Home({ allPostsData }) {
           Hi, I'm <strong>Mauro</strong>. I'm an advance student of Information
           Systems Engineering with interest in frontend development.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
